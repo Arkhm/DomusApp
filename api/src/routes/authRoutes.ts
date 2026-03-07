4d36138b47b92c3ab798e23f30cd8efa406c1ec6
Erick <ericknprs@gmail.com>
2026-03-07 20:04:03 -0300

    :sparkles: implementar middleware RBAC e rota protegida para admin

--- api/src/routes/authRoutes.ts
+++ api/src/routes/authRoutes.ts
@@ -1,9 +1,24 @@
 import { Router } from 'express';
 import { authController } from '../controllers/authController';
+import { authMiddleware } from '../middlewares/authMiddleware';
+import { authorizeRole } from '../middlewares/roleMiddleware';
 
 const router = Router();
 
+// Rotas Públicas
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+
+// Rotas Protegidas
+// Criar novo Administrador
+// 1. O authMiddleware verifica o token JWT e injeta o req.user
+// 2. O authorizeRole verifica se o req.user.role é "ADMIN"
+// 3. Só então o controlador é chamado
+router.post(
+  '/register-admin', 
+  authMiddleware, 
+  authorizeRole(['ADMIN']), 
+  authController.registerAdmin
+);
 
 export default router;
