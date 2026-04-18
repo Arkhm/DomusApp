2ec332ce8bf232005763bd4d5d509b0ea6a73359
Erick <ericknprs@gmail.com>
2026-04-18 19:45:06 -0300

    :lock: Adicionada camada de segurança para impedir que o usuário possa enviar qualquer valor para Role

--- api/src/services/userService.ts
+++ api/src/services/userService.ts
@@ -26,10 +26,20 @@ export const userService = {
     if (await userRepository.findByEmail(data.email)) throw new Error('E-mail já cadastrado.');
     if (await userRepository.findByCpf(data.cpf)) throw new Error('CPF já cadastrado.');
 
+    // Camada de proteção para impedir inserçãoo de roles não permitidas
+    const ALLOWED_ROLES = ['ADMIN', 'FUNCIONARIO', 'MORADOR'];
+
     if (data.role) {
-      data.role = data.role.toUpperCase();
-    } else {
-      data.role = 'MORADOR'; 
+          data.role = data.role.toUpperCase();
+          if (!ALLOWED_ROLES.includes(data.role)) {
+            throw new Error(`Cargo inválido. Use apenas: ${ALLOWED_ROLES.join(', ')}`);
+          }
+        } else {
+          data.role = 'MORADOR';
+        }
+    // Para que um morador não seja criado sem uma unidade
+    if (data.role === 'MORADOR' && !data.unitId) {
+      throw new Error('É obrigatório informar a unidade (unitId) para cadastrar um morador.');
     }
 
     if (data.role !== 'MORADOR') {
@@ -57,8 +67,18 @@ export const userService = {
       if (await userRepository.findByCpf(data.cpf)) throw new Error('CPF já cadastrado por outro.');
     }
 
+    // Camada de proteção para impedir inserçãoo de roles não permitidas
+    const ALLOWED_ROLES = ['ADMIN', 'FUNCIONARIO', 'MORADOR'];
+
     if (data.role) {
       data.role = data.role.toUpperCase();
+      if (!ALLOWED_ROLES.includes(data.role)) {
+        throw new Error(`Cargo inválido. Use apenas: ${ALLOWED_ROLES.join(', ')}`);
+      }
+    }
+
+    if (data.role === 'MORADOR' && data.unitId === null) {
+        throw new Error('Um morador não pode ficar sem unidade.');
     }
 
     if (data.role && data.role !== 'MORADOR') {
